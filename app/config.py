56422bda790b21e1697a38301c0160056afdbd9d
Michael DiGiovanni <midigi@comcast.net>
2021-02-22 15:46:06 -0500

    updates

--- app/config.py
+++ app/config.py
@@ -1,17 +1,18 @@
 import os
 
+
 class Config:
-  SECRET_KEY=os.environ.get('SECRET_KEY')
-  SQLALCHEMY_TRACK_MODIFICATIONS=False
-  SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
-  SQLALCHEMY_ECHO=True
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_ECHO = True
 
-class Aws3:
-  S3_BUCKET=os.environ.get("asauna-user-images")
-  S3_KEY=os.environ.get("AKIAIBLVQSFMM2EWIQTQ")
-  S3_SECRET=os.environ.get("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")
-  S3_LOCATION='http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
 
-  SECRET_KEY=os.urandom(32)
-  DEBUG=True
-  PORT=5000
+S3_BUCKET = os.environ.get("asauna-user-images")
+S3_KEY = os.environ.get("AKIAIBLVQSFMM2EWIQTQ")
+S3_SECRET = os.environ.get("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")
+S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
+
+SECRET_KEY = os.urandom(32)
+DEBUG = True
+PORT = 5000
--- app/helpers.py
+++ app/helpers.py
@@ -1,12 +1,11 @@
 import boto3
 import botocore
-from .config import Aws3
+from .config import S3_KEY, S3_SECRET, S3_BUCKET
 
 s3 = boto3.client(
     "s3",
-    #    This might be written wrong. I'm not sure if we can dot into the class Aws3 like this
-    aws_access_key_id=Aws3.S3_KEY,
-    aws_secret_access_key=Aws3.S3_SECRET
+    aws_access_key_id=S3_KEY,
+    aws_secret_access_key=S3_SECRET
 )
 
 
@@ -21,10 +20,10 @@ def upload_file_to_s3(file, bucket_name, acl="public-read"):
                 "ContentType": file.content_type
             }
         )
-        
+
     except Exception as e:
         # This is a catch all exception, edit this part to fit your needs.
         print("Something Happened: ", e)
         return e
 
-    return "{}{}".format(app.config["S3_LOCATION"], file.filename)
+    return "{}{}".format("S3_LOCATION", file.filename)
