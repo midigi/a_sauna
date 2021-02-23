b62bff42f0f19f7397f6c9b00c7fcbad1d543e9c
Andre Grant <Iamdgrant@me.com>
2021-02-22 22:14:03 -0500

    configured session thunk

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,26 +1,50 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
-import { signUp } from "../../services/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { createUser } from "../../store/session";
+import { signUp } from "./SignUpForm";
 import "./authStyling/form.css";
 
 const SignUpForm = ({ authenticated, setAuthenticated }) => {
-  const [username, setUsername] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
+  // const [errors, setErrors] = useState([]);
+
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.session.user);
 
   const onSignUp = async (e) => {
     e.preventDefault();
+    let newErrors = [];
     if (password === repeatPassword) {
-      const user = await signUp(username, email, password);
+      dispatch(createUser({ firstName, lastName, email, password }))
+        .then(() => {
+          setFirstName("");
+          setLastName("");
+          setEmail("");
+          setPassword("");
+        })
+        .catch(async (res) => {
+          const data = await res.json();
+          if (data && data.errors) {
+            newErrors = data.errors;
+            // setErrors(newErrors);
+          }
+        });
       if (!user.errors) {
         setAuthenticated(true);
       }
     }
   };
 
-  const updateUsername = (e) => {
-    setUsername(e.target.value);
+  const updateFirstName = (e) => {
+    setFirstName(e.target.value);
+  };
+  const updateLastName = (e) => {
+    setLastName(e.target.value);
   };
 
   const updateEmail = (e) => {
@@ -55,10 +79,20 @@ const SignUpForm = ({ authenticated, setAuthenticated }) => {
           <input
             className="form_input"
             type="text"
-            name="username"
-            placeholder="User Name"
-            onChange={updateUsername}
-            value={username}
+            name="firstName"
+            placeholder="First Name"
+            onChange={updateFirstName}
+            value={firstName}
+          ></input>
+        </div>
+        <div>
+          <input
+            className="form_input"
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+            onChange={updateLastName}
+            value={lastName}
           ></input>
         </div>
         <div>
