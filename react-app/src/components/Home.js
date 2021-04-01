d7a79c31fec5f572a57662d5ac9420b2974e0bb0
Antonio <anguilar@aol.com>
2021-04-01 11:47:07 -0700

    styling members

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -9,19 +9,39 @@ const { Panel } = Collapse;
 
 function Home() {
   return (
-    <div className="main_content">
-      <Collapse bordered={false} className="collapse">
-        <Panel header="Tasks due soon">
-          <TaskHeaders></TaskHeaders>
-          <Task></Task>
-        </Panel>
-      </Collapse>
-      <Collapse defaultActiveKey={["1"]} bordered={false} className="collapse">
-        <Panel header="Recent Projects" key="1">
-          <RecentProjects></RecentProjects>
-        </Panel>
-      </Collapse>
-    </div>
+    <>
+      <div className="main_content">
+        <Collapse bordered={false} className="collapse">
+          <Panel header="Tasks due soon">
+            <TaskHeaders></TaskHeaders>
+            <Task></Task>
+          </Panel>
+        </Collapse>
+        <Collapse
+          defaultActiveKey={["1"]}
+          bordered={false}
+          className="collapse"
+        >
+          <Panel header="Recent Projects" key="1">
+            <RecentProjects></RecentProjects>
+          </Panel>
+        </Collapse>
+      </div>
+      <div className="footer">
+        <a href="https://github.com/vantanova" className="dev_link">
+          Antonio A.
+        </a>
+        <a href="https://github.com/IamDgrant" className="dev_link">
+          Andre G.
+        </a>
+        <a href="https://github.com/bparsons17" className="dev_link">
+          Brandon P.
+        </a>
+        <a href="https://github.com/midigi" className="dev_link">
+          Micheal D.
+        </a>
+      </div>
+    </>
   );
 }
 export default Home;
